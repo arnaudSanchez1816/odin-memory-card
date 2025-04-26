d2f018db8c837fd4f127b63b5755e7f4a35d3865
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-04-26 22:32:28 +0200

    Remove not needed cit config change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
     plugins: [react()],
     base: "/odin-memory-card/",
-    build: {
-        assetsInlineLimit: 0,
-    },
 })
